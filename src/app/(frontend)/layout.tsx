02d534162abf4a598a49db64a207fbc686db1f4a
Jim Groome <jim.groome@brego.io>
2026-04-13 13:38:22 +0100

    Our routes should rehydrate when changes are saved

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,19 +4,30 @@ import PageHeader from '@/components/page-header/PageHeader'
 import { PageFooter } from '@/components/page-footer/PageFooter'
 import { getPayload } from 'payload'
 import config from '@/payload.config'
+import { unstable_noStore as noStore } from 'next/cache'
 
-const payload = await getPayload({ config })
-const branding = await payload.findGlobal({
-  slug: 'branding',
-})
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
-export const metadata = {
-  description:
-    (branding?.description as string) || 'A blank template using Payload in a Next.js app.',
-  title: (branding?.name as string) || 'My Cranbrook',
+export async function generateMetadata() {
+  noStore()
+
+  const payload = await getPayload({ config })
+  const branding = await payload.findGlobal({
+    slug: 'branding',
+  })
+
+  return {
+    description:
+      (branding?.description as string) || 'A blank template using Payload in a Next.js app.',
+    title: (branding?.name as string) || 'My Cranbrook',
+  }
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  noStore()
+
   return (
     <html lang="en">
       <body>
